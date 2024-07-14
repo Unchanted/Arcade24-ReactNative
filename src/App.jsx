410ff79451df71e641eed246bb1dfd3b5b1505fc
Unchanted <omkkdwivedi@gmail.com>
2024-07-14 22:42:58 +0530

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignupPage} />
-        <Route path="/" component={LoginPage} />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </Router>
   );
 };
